f6f98358bcb5dd79a6969425f34a9cd3a5b2e76a
BesBlet <45937663+BesBlet@users.noreply.github.com>
2020-10-20 18:07:37 +0300

    Initial commit

---
+++ Assets/Scripts/ButtonScene.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonScene : MonoBehaviour
+{
+    public void LoadNextLevel()
+    {
+        int activeSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        SceneManager.LoadScene(activeSceneIndex + 1);
+    }
+
+    public void LoadFirstScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void GameOverScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
+
